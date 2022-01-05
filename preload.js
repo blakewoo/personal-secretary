e13fac62d8017d74c014cdafc4ace5abe0d3c6ae
Blake <blakewoo0819@gmail.com>
2022-01-05 20:22:10 +0900

    add event & modify css (hover)

---
+++ preload.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    addCategoryButtonEvent()
+})
+
+function addCategoryButtonEvent() {
+    document.getElementById("add_category").addEventListener("click",function (event){
+        alert("ddd")
+    })
+
+    document.getElementById("modify_category").addEventListener("click",function (event){
+        alert("ddd")
+    })
+
+    document.getElementById("delete_category").addEventListener("click",function (event){
+        alert("ddd")
+    })
+}
+
