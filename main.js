46756caea148ff20b3c108b50fa6903aa622578c
Blake <blakewoo0819@gmail.com>
2022-01-08 12:50:12 +0900

    add signup page & find account page

--- main.js
+++ main.js
@@ -16,6 +16,36 @@ function createLoginWindow() {
     mainWindow.loadFile('login.html')
 }
 
+function createSignupWindow() {
+    // Create the browser Signup window.
+    const mainWindow = new BrowserWindow({
+        width: 300,
+        height: 400,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'signupPreload.js')
+        },
+        title:"Sign up"
+    })
+    mainWindow.setResizable(false);
+    mainWindow.loadFile('signup.html')
+}
+
+function createFindAccount() {
+    // Create the browser find account window.
+    const mainWindow = new BrowserWindow({
+        width: 300,
+        height: 400,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'find_accountPreload.js')
+        },
+        title:"Find your account"
+    })
+    mainWindow.setResizable(false);
+    mainWindow.loadFile('find_account.html')
+}
+
 
 function createWindow () {
     // Create the browser window.
