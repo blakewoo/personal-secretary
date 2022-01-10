6dfdc6782d426412c0a94db5d0b3bf6e854a560d
Blake <blakewoo0819@gmail.com>
2022-01-10 19:40:49 +0900

    modify file structure & modify login layout

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ function createLoginWindow() {
         height: 400,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'loginPreload.js')
+            preload: path.join(__dirname, 'preload/loginPreload.js')
         },
         title:"Login to timeout"
     })
     mainWindow.setResizable(false);
-    mainWindow.loadFile('login.html')
+    mainWindow.loadFile('html/login.html')
 }
 
 function createSignupWindow() {
@@ -23,12 +23,12 @@ function createSignupWindow() {
         height: 400,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'signupPreload.js')
+            preload: path.join(__dirname, 'preload/signupPreload.js')
         },
         title:"Sign up"
     })
     mainWindow.setResizable(false);
-    mainWindow.loadFile('signup.html')
+    mainWindow.loadFile('html/signup.html')
 }
 
 function createFindAccount() {
@@ -38,12 +38,12 @@ function createFindAccount() {
         height: 400,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'find_accountPreload.js')
+            preload: path.join(__dirname, 'preload/find_accountPreload.js')
         },
         title:"Find your account"
     })
     mainWindow.setResizable(false);
-    mainWindow.loadFile('find_account.html')
+    mainWindow.loadFile('html/find_account.html')
 }
 
 
@@ -54,15 +54,15 @@ function createWindow () {
         height: 600,
         autoHideMenuBar: true,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload/preload.js')
         }
     })
 
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('html/index.html')
 }
 
 app.whenReady().then(() => {
-    createSignupWindow()
+    createLoginWindow()
 
     app.on('activate', function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
