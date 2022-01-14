d03e1c4f5fd1a2400f68c37af1958aee0de29f75
Blake <blakewoo0819@gmail.com>
2022-01-14 21:03:33 +0900

    fix header layout

--- preload/loginPreload.js
+++ preload/loginPreload.js
@@ -1,13 +1,9 @@
 
 
-const remote = require('electron').remote;
 
-// function window_close() {
-//     currentWindow.close()
-// }
+window.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.getElementById('close_button')
+    closeBtn.addEventListener('click', function (event) {
 
-document.getElementById("close_button").addEventListener("click",function (event){
-    // window_close()
-    let win = remote.getCurrentWindow();
-    win.close()
+    })
 })
