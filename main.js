af6b689ab9f9b40508a3ea8186eeea6a40155f70
Blake <blakewoo0819@gmail.com>
2022-01-26 19:31:38 +0900

    add routing logic

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const {app, BrowserWindow } = require('electron')
 
-const path = require('path')
 const {ipcMain} = require('electron');
 const mainFunction = require('./module/mainModule');
 const loginFunction = require('./module/loginModule');
 const signupFunction = require('./module/signupModule');
 const findIdFunction = require('./module/findidModule');
 
-
 app.whenReady().then(() => {
     let loginWindow = loginFunction.createLoginWindow()
 
     ipcMain.on('loginButtonEvent', (event,arg) => {
         if (arg.value === "login") {
-            loginWindow.close()
-            mainFunction.createWindow()
+            let id = arg.ID
+            let pass = arg.PASSWORD
+
+            if(IsLogin(id,pass)) {
+                loginWindow.close()
+                mainFunction.createWindow()
+            }
+            else {
+
+            }
         }
         else if (arg.value === "signup") {
             loginWindow.close()
@@ -35,4 +41,9 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
+
+function IsLogin(id,pass) {
+    console.log(id,pass)
+    return true
+}
 
--- preload/loginPreload.js
+++ preload/loginPreload.js
@@ -8,15 +8,15 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById("signup_button").addEventListener('click', signUpClickEvent)
     document.getElementById("findpass_button").addEventListener('click', findClickEvent)
 
-
     function closeClickEvent (event) {
-
         ipcRenderer.send('loginFrameButtonEvent',{value:"close"});
-
     }
 
     function loginClickEvent(event) {
-        ipcRenderer.send('loginButtonEvent',{value:"login"});
+        let id = document.getElementById("user_input").value;
+        let pass = document.getElementById("password_input").value;
+
+        ipcRenderer.send('loginButtonEvent',{value:"login",ID:id,PASSWORD:pass});
     }
 
     function signUpClickEvent(event) {
