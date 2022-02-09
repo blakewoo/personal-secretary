1103e183f8a8b0c5ab0f12864794d77e057c04c0
Blake <blakewoo0819@gmail.com>
2022-02-09 20:00:16 +0900

    modify structure (uncompleted)

--- preload/loginPreload.js
+++ preload/loginPreload.js
@@ -1,18 +1,41 @@
 
 const {ipcRenderer} = require('electron')
 window.addEventListener('DOMContentLoaded', () => {
-
-    ipcRenderer.on('set-login-page',(event, arg) => {
-        loadLoginPage ()
-    })
-
-    ipcRenderer.on('set-signup-page',(event, arg) => {
-        loadSignupPage ()
-    })
-
+    loadLoginPage()
 })
 
-function loadLoginPage () {
+function loadLoginPage() {
+    let str = "    <h1 class=\"head_title\">일잘러의 수첩</h1>\n" +
+        "    <div class=\"att_border_bottom\">\n" +
+        "        <label>사용자 : </label><input class=\"input_border\" id=\"user_input\" type=\"text\">\n" +
+        "    </div>\n" +
+        "    <div class=\"att_border_bottom\">\n" +
+        "        <label>비밀번호 : </label><input class=\"input_border\" id=\"password_input\" type=\"text\">\n" +
+        "        <div>\n" +
+        "            <label class=\"notice_wrong_account\" id=\"wrong_account\">\n" +
+        "                아이디 혹은 패스워드가 올바르지 않습니다.\n" +
+        "            </label>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "\n" +
+        "    <div class=\"att_border_bottom\">\n" +
+        "        <input type=\"button\" value=\"로그인\" class=\"public_button\" id=\"login_button\"/>\n" +
+        "    </div>\n" +
+        "\n" +
+        "\n" +
+        "    <div class=\"att_border_bottom\">\n" +
+        "        <input type=\"button\" value=\"새로 수첩 만들기\" class=\"public_button\" id=\"signup_button\"/>\n" +
+        "    </div>\n" +
+        "    <div class=\"att_border_bottom\">\n" +
+        "        <input type=\"button\" value=\"수첩 찾기\" class=\"public_button\" id=\"findpass_button\"/>\n" +
+        "    </div>"
+
+    document.getElementById("main_html_div").innerHTML = str
+
+    bindLoginEvent ()
+}
+
+function bindLoginEvent () {
     document.getElementById("close_button").addEventListener('click', closeClickEvent)
     document.getElementById("login_button").addEventListener('click', loginClickEvent)
     document.getElementById("signup_button").addEventListener('click', signUpClickEvent)
@@ -33,15 +56,21 @@ function loadLoginPage () {
     }
 
     function signUpClickEvent(event) {
-        ipcRenderer.send('loginButtonEvent',{value:"signup"});
+
     }
 
     function findClickEvent(event) {
-        ipcRenderer.send('loginButtonEvent',{value:"findid"});
+
     }
 }
 
 function loadSignupPage() {
+
+
+    bindSignupEvent()
+}
+
+function bindSignupEvent() {
     document.getElementById("close_button").addEventListener('click', closeClickEvent)
     document.getElementById("accept_button").addEventListener('click', acceptClickEvent)
     document.getElementById("cancel_button").addEventListener('click', cancelClickEvent)
