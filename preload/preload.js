c2f818d376413c04b76ca1942ff62d103c64fa26
Blake <blakewoo0819@gmail.com>
2022-02-17 20:43:40 +0900

    change Category type

--- preload/preload.js
+++ preload/preload.js
@@ -6,20 +6,34 @@ window.addEventListener('DOMContentLoaded', () => {
         ipcRenderer.send('mainFrameButtonEvent',{value:"close"});
     }
     addCategoryButtonEvent()
+    addCategoryEvent()
 })
 
 function addCategoryButtonEvent() {
 
     document.getElementById("add_category").addEventListener("click",function (event){
-        ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
+        // ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
     })
 
     document.getElementById("modify_category").addEventListener("click",function (event){
-        ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
+        // ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
     })
 
     document.getElementById("delete_category").addEventListener("click",function (event){
-        ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
+        // ipcRenderer.send('mainPageButtonEvent',{value:"signup"});
     })
+
+}
+
+function addCategoryEvent() {
+    let category = document.getElementsByClassName("category_label")
+    for (let i =0 ;i<category.length;i++) {
+        category[i].removeEventListener("click",categoryClickEvent)
+        category[i].addEventListener("click",categoryClickEvent)
+    }
+
+    function categoryClickEvent(event) {
+        event.currentTarget
+    }
 }
 
