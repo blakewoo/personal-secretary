cee5b29208ba05e7313e5abf5b62e652571f9b06
Blake <blakewoo0819@gmail.com>
2022-02-25 20:08:32 +0900

    update main page(checkbox)

--- preload/preload.js
+++ preload/preload.js
@@ -17,7 +17,7 @@ function addTodoDetailEvent() {
     add_todo.addEventListener("keyup",function (event) {
         if(event.key === "Enter") {
             let categoryContainer = document.getElementsByClassName("todo_detail_top")[0]
-            categoryContainer.innerHTML += "<img src='./../images/empty_checkbox.png' class='unchecked_checkbox'><label class='category_detail'>"+add_todo.value+"</label>"
+            categoryContainer.innerHTML += "<span class='unchecked_checkbox'></span><label class='category_detail'>"+add_todo.value+"</label>"
             todoDetailEventBinder()
         }
     })
@@ -28,19 +28,21 @@ function todoDetailEventBinder(){
     let todo_checkbox_uncheck = document.getElementsByClassName("unchecked_checkbox")
     let todo_checkbox_check = document.getElementsByClassName("checked_checkbox")
     for (let i =0;i<todo_checkbox_check.length;i++) {
+        todo_checkbox_check[i].removeEventListener("click",todoCheckedDetailEvent)
         todo_checkbox_check[i].addEventListener("click",todoCheckedDetailEvent)
     }
 
     for (let i =0;i<todo_checkbox_uncheck.length;i++) {
+        todo_checkbox_uncheck[i].removeEventListener("click",todoUncheckedDetailEvent)
         todo_checkbox_uncheck[i].addEventListener("click",todoUncheckedDetailEvent)
     }
 }
 function todoCheckedDetailEvent(event) {
-    event.currentTarget.src = "./../images/empty_checkbox.png"
+    event.currentTarget.classList.add("check")
 }
 
 function todoUncheckedDetailEvent(event) {
-    event.currentTarget.src = "./../images/checked_checkbox.png"
+    event.currentTarget.classList.remove("check")
 }
 
 
